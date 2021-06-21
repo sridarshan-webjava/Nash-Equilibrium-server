27addc69346fb433276e82a9474930ac9a107701
sridarshan <sridarshan1997@gmail.com>
2021-06-21 20:39:31 +0530

    Game play changed to 4 players and fixing code bugs

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ const io = require("socket.io")(server, {
   },
 });
 
-let noOfPlayers = 2;
+let noOfPlayers = 4;
 let gameStarted = false;
 let players = [];
-let currentRound = 1;
+let currentRound = 0;
 let noOfResponses = 0;
 let responses = {
   2: [],
@@ -30,18 +30,27 @@ let amountGiven = {
   2: 0,
   1: 0,
 };
-let map = new Map();
 
 app.use(cors());
 
 function updateScores() {
-  if (responses[2].length === 2 && responses[1].length === 0) {
+  if (responses[2].length === 4 && responses[1].length === 0) {
     amountGiven[2] = -25;
-  } else if (responses[2].length === 1 && responses[1].length === 1) {
+    amountGiven[1] = 0;
+  } else if (responses[2].length === 3 && responses[1].length === 1) {
     amountGiven[2] = 25;
+    amountGiven[1] = -25;
+  } else if (responses[2].length === 2 && responses[1].length === 2) {
+    amountGiven[2] = 50;
+    amountGiven[1] = -12.5;
+  } else if (responses[2].length === 1 && responses[1].length === 3) {
+    amountGiven[2] = 75;
+    amountGiven[1] = 0;
   } else {
+    amountGiven[2] = 0;
     amountGiven[1] = 25;
   }
+
   players = players.map(player => {
     if (responses[2].includes(player.id)) {
       player.score += amountGiven[2];
@@ -50,8 +59,21 @@ function updateScores() {
     }
     return player;
   });
+
   responses[2] = [];
   responses[1] = [];
+}
+
+function findWinner() {
+  let maxScore = 0;
+  const winner = players.reduce((acc, player) => {
+    if (player.score > maxScore) {
+      maxScore = player.score;
+      acc = player.playerData;
+    }
+    return acc;
+  }, "");
+  return winner;
 }
 
 io.on("connection", socket => {
@@ -71,18 +93,29 @@ io.on("connection", socket => {
       io.emit("waiting", "Waiting for players");
     } else if (noOfPlayers === 0) {
       io.emit("start", "Starting Game");
-      io.emit("next-round", currentRound);
+      io.emit("next-round", currentRound + 1);
       gameStarted = true;
     }
   }
   socket.on("play-round", resp => {
     responses[resp].push(socket.id);
     noOfResponses++;
-    if (noOfResponses === 2) {
+    if (noOfResponses === 8) {
       updateScores();
       noOfResponses = 0;
-      io.emit("update-scores", players);
-      io.emit("next-round", ++currentRound);
+      currentRound++;
+
+      if (currentRound === 3) {
+        io.emit("update-scores", players);
+        const winner = findWinner();
+        io.emit("end-game", `${winner} is the winner !!`);
+        gameStarted = false;
+        players = [];
+        currentRound = 0;
+      } else {
+        io.emit("update-scores", players);
+        io.emit("next-round", currentRound + 1);
+      }
     }
   });
 });
